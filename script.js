d63cbbed8493e074a24a2aa35c36a754fb954037
uzmamstf <49521264+uzmamstf@users.noreply.github.com>
2019-06-25 15:06:10 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,58 @@
-/*
-- sb se pehle mjhe price input ki value get krni hai
-- phir mjhe usko total price k span me show krna hai
-
-*/
-// function showInputValue() {
-//     let getValueOfInputAndShow = document.querySelector('#totalPriceSpan').innerHTML =
-//         document.querySelector('#myPriceInput').value;
-// }
-
-/*
-let input = document.getElementById("myPriceInput");
-let span = document.getElementById("totalPriceSpan");
-
-input.addEventListener("input", () => {
-    document.getElementById('totalPriceSpan').innerHTML = input.value;
-});
-*/
-// third method
-// get value form input and show in span
-let input = document.querySelector('#myPriceInput'); //for first input 
+let input =0 , app_purchase= 0,ban_purchase=0,org_purchase=0,quant_app=0;
+function getval(){
+input = document.querySelector("input").value; //for first input 
+}
 let span = document.querySelector('#totalPriceSpan'); //remaining rs
-
 let appleRs = document.querySelector('#numberapple'); // price apple
 let bananaRs = document.querySelector('#numberbanana'); //price babana
 let orangeRs = document.querySelector('#numberorange'); //price orange
-
 let appleMultiRupees = 10 * appleRs; 
 let bananaMultiRupees = 15 * bananaRs; 
 let orangeMultiRupees = 07 * orangeRs; 
-
-
-
 //apple
 function appleincrement() {
-    let quan = document.querySelector("#numberapple").value++;
+    var quant_app = document.querySelector("#numberapple").value++;
+    app_purchase = app_purchase + 10; 
+    show();
     
 }
 function appledecrease() {
-    let quan = document.querySelector("#numberapple").value--;
+    let quan_app = document.querySelector("#numberapple").value--;
+    app_purchase = app_purchase - 10; 
+    show();
+    
 }
-
 //banana
 function bananaincrement() {
-    let quan = document.querySelector("#numberbanana").value++;
+    let quan_banana = document.querySelector("#numberbanana").value++;
+    ban_purchase = ban_purchase + 7; 
+    show();
     
 }
 function bananadecrease() {
-    let quan = document.querySelector("#numberbanana").value--;
+    let quan_banana = document.querySelector("#numberbanana").value--;
+    ban_purchase = ban_purchase - 7; 
+    show();
 }
 
 //orange
 function orangeincrement() {
-    let quan = document.querySelector("#numberorange").value++;
+    let quan_org = document.querySelector("#numberorange").value++;
+    org_purchase = org_purchase + 15; 
+    show();
     
 }
 function orangedecrease() {
-    let quan = document.querySelector("#numberorange").value--;
+    let quan_org = document.querySelector("#numberorange").value--;
+    org_purchase = org_purchase - 15; 
+    show();
 }
 
 // alert(span.innerHTML);
  function show() {
-
-    let totalbill = appleMultiRupees +  bananaMultiRupees + orangeMultiRupees;
-    let netremainingval = input - totalbill;
-    span.innerHTML = netremainingval.value;
+    appleMultiRupees =1; bananaMultiRupees=1;orangeMultiRupees=1;
+    var totalbill = app_purchase + ban_purchase + org_purchase;
+    document.querySelector("span").innerHTML = totalbill;
 }
 
 // input.addEventListener("input", () => {
